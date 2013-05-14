a9b55a9fedd8134c1db1343a50c487bbd06a0c9f
baconstarvation <ant.ucd@gmail.com>
2013-05-13 23:45:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsOm::Application.config.secret_token = '6f64f010e711ede56933d59254a5ddb43f9f84ee69d839650a0152d146a746e064c1ae65ecaddc180cae47285ed7ec7e2dab96fa906ab5f026df670ab72c7a52'
